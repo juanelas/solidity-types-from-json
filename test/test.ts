4711bed02dbe990e62b11d3622c0c34f500a67d9
juanelas <j.hernandez@upc.edu>
2024-07-19 16:11:02 +0200

    pass coverage

--- test/test.ts
+++ test/test.ts
@@ -52,4 +52,89 @@ describe(`testing ${_MODULE_TYPE}-module function jsonToSolidityTypes(obj)`, fun
     const { types } = jsonToSolidityTypes(exampleObject)
     chai.expect(JSON.stringify(types) === JSON.stringify(expectedTypes), JSON.stringify(types, null, 2)).to.be.true // eslint-disable-line
   })
+  it('should properly use mainTypeName', function () {
+    const { types } = jsonToSolidityTypes(exampleObject, { mainTypeName: 'JWT' })
+    const newlyExpectedTypes: any = { ...expectedTypes }
+    newlyExpectedTypes.JWT = newlyExpectedTypes.Type
+    delete newlyExpectedTypes.Type
+    chai.expect(JSON.stringify(types) === JSON.stringify(newlyExpectedTypes), JSON.stringify(types, null, 2)).to.be.true // eslint-disable-line
+  })
+  it('should properly use getTypeNamesFromKeyValues', function () {
+    const { types } = jsonToSolidityTypes(exampleObject, { getTypeNamesFromKeyValues: false })
+    const newlyExpectedTypes = {
+      Type: [
+        {
+          name: 'a',
+          type: 'uint256'
+        },
+        {
+          name: 'b',
+          type: 'bool'
+        }
+      ],
+      Type2: [
+        {
+          name: 'street',
+          type: 'string'
+        },
+        {
+          name: 'city',
+          type: 'string'
+        },
+        {
+          name: 'arr',
+          type: 'uint256[]'
+        },
+        {
+          name: 'obj',
+          type: 'Type'
+        }
+      ],
+      Type3: [
+        {
+          name: 'key',
+          type: 'string'
+        },
+        {
+          name: 'value',
+          type: 'string'
+        }
+      ],
+      Type4: [
+        {
+          name: 'name',
+          type: 'string'
+        },
+        {
+          name: 'age',
+          type: 'uint256'
+        },
+        {
+          name: 'isActive',
+          type: 'bool'
+        },
+        {
+          name: 'ethAddress',
+          type: 'address'
+        },
+        {
+          name: 'address',
+          type: 'Type2'
+        },
+        {
+          name: 'scores',
+          type: 'uint256[]'
+        },
+        {
+          name: 'preferences',
+          type: 'Type3[]'
+        },
+        {
+          name: 'arrAlt',
+          type: 'Type[]'
+        }
+      ]
+    }
+    chai.expect(JSON.stringify(types) === JSON.stringify(newlyExpectedTypes), JSON.stringify(types, null, 2)).to.be.true // eslint-disable-line
+  })
 })
